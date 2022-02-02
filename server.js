bec2389060641de63186b4fd951009ce66ce7868
Deleon Simoni <deleon.simoni@gmail.com>
2022-02-02 01:50:05 -0300

    banco de prd apontado

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ var http = require('http');
 var app = require('./config/express');
 require('dotenv').config();
 var porta = 3001
-require('./config/database')('localhost/vetzdes');
-/*require('./config/database')('mongo71-farm10.kinghost.net/vetzco01', 
-                                {   user: 'vetzco01',
-                                    pass: 'Ad=15937-Bcng',
-                                    useNewUrlParser: true,
-                                    useUnifiedTopology: true 
-                                });*/
+//require('./config/database')('localhost/vetzdes');
+require('./config/database')('mongo71-farm10.kinghost.net/vetzco01',
+    {
+        user: 'vetzco01',
+        pass: 'Ad=15937-Bcng',
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
 
 http.createServer(app).listen(porta, function () {
     console.log('Server Start - V.1.11 - Porta - ' + porta);
