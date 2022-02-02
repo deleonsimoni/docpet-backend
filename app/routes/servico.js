0d14c61838242cda0c384164ee16c345bd200361
Deleon Simoni <deleon.simoni@gmail.com>
2022-02-02 01:49:22 -0300

    vinculação email aos usuarios remanescentes

--- app/routes/servico.js
+++ app/routes/servico.js
@@ -1,18 +1,20 @@
-module.exports= function(app){
-    
+module.exports = function (app) {
+
     var api = app.api.servico;
 
     app.route('/v1/servicos')
-    .get(api.lista)
-    .post(api.adicionaServico)
+        .get(api.lista)
+        .post(api.adicionaServico)
 
     app.route('/v1/servicos/:id')
-    .put(api.atualizaServico)
-    .delete(api.removeServicoId)
-    .get(api.buscaPorId);
+        .put(api.atualizaServico)
+        .delete(api.removeServicoId)
+        .get(api.buscaPorId);
 
+    app.route('/v1/servicos/merge/usersVets')
+        .get(api.mergeUsersVets);
 
-    
- 
-   
+    app.route('/v1/servicos/merge/usersEstabs')
+        .get(api.mergeUsersEstabs);
+
 }
